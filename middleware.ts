93a696ac93618fd5adce42d1ac4cf4bde8f2d8a8
EverChan6 <EverChan611@Gmail.com>
2025-09-18 14:07:18 +0800

    fix: middleware runtime nodejs

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,5 @@ export default NextAuth(authConfig).auth;
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  // runtime: 'nodejs', // 报错：Error: Cannot find the middleware module，原因貌似：Next.js 15.3.2 与 NextAuth v5.0.0-beta.25/v5.0.0-beta.29 在 middleware runtime 上的兼容性问题
+  runtime: 'nodejs',
 };
